4a416dbe7790ff6a46dd406074cb54eda418e2b3
Eflex <158610374+digitaleflex@users.noreply.github.com>
2025-06-15 20:52:38 +0100

    chapitre 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,69 +6,80 @@ import postgres from 'postgres';
 import { redirect } from 'next/navigation';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
- 
- 
+
+
 const FormSchema = z.object({
-  id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
-  date: z.string(),
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
 });
- 
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 
 
 export async function createInvoice(formData: FormData) {
-  const rawCustomerId = formData.get('customerId');
-  const rawAmount = formData.get('amount');
-  const rawStatus = formData.get('status');
+    const rawCustomerId = formData.get('customerId');
+    const rawAmount = formData.get('amount');
+    const rawStatus = formData.get('status');
 
-  if (!rawCustomerId || !rawAmount || !rawStatus) {
-    throw new Error("Champs requis manquants.");
-  }
+    if (!rawCustomerId || !rawAmount || !rawStatus) {
+        throw new Error("Champs requis manquants.");
+    }
 
-  const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: rawCustomerId,
-    amount: rawAmount,
-    status: rawStatus,
-  });
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: rawCustomerId,
+        amount: rawAmount,
+        status: rawStatus,
+    });
 
-  const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+    try {
+        await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+    } catch (error) {
+        // We'll log the error to the console for now
+        console.error(error);
+    }
 
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
 
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
- 
-  const amountInCents = amount * 100;
- 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
- 
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    try {
+        await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+    } catch (error) {
+        // We'll log the error to the console for now
+        console.error(error);
+    }
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+    throw new Error('Failed to Delete Invoice');
+
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
 }
